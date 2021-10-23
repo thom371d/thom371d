9f0e32d896b2a12f13b830a2f2d9a7b15970577a
Thomas Freddy Almstrup larsen <tfal@Thomass-MBP.domain_not_set.invalid>
2021-10-23 22:58:18 +0200

    Added main

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO; 
+using System.Drawing;
+using System.Drawing.Printing;
+using QRCoder; 
+using thom371d;
+
+     
+// Read Console input stream: https://stackoverflow.com/questions/50279001/how-to-read-console-input-until-space-or-enter-in-c-sharp
+
+namespace thom371d
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        
+            simpleFunctions myFactory = new simpleFunctions(); 
+            
+
+            using (var sr = new StreamReader(Console.OpenStandardInput(), Console.InputEncoding)) // Opens streamreader, takes input in terminal 
+            {
+                var input = sr.ReadToEnd();
+
+                var tokens = input.Replace(Environment.NewLine, " ").Split(" ");
+
+                foreach (var t in tokens)
+                {
+                    Console.WriteLine($"Token: \"{t}\"");
+                }
+
+                // var terminalInput = Console.Read();
+                var finalQR = myFactory.createDynamicQRCode(input);
+                
+                Bitmap qrCodeImage = finalQR.GetGraphic(20);
+                
+            }
+        }
+    }
+}
+
+
